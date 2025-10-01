1540799879bfc44254ab23474f508f0f1abcaa41
sobhika11 <sobhika1105@gmail.com>
2025-10-01 20:45:08 +0530

    Conncted with db

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,9 @@
-mongosh "mongodb+srv://cluster0.u7yormy.mongodb.net/" --apiVersion 1 --username Sobhika_11 --password Kanisobhi@1130
+const express=require('express');
+const app=express();
+app.get('/',(rer,res)=> 
+res.send("fitness backend is running")
+);
+const PORT=process.env.PORT || 5000;
+app.listen(PORT, ()=>console.log("server is running on port http://localhost:5000/ "
+    
+)); //to start express server
