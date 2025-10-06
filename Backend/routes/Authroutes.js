1e9a9edf2b59b149c8b606d891a6bf7bc570e634
sobhika11 <sobhika1105@gmail.com>
2025-10-06 23:33:50 +0530

    Completed login and sigup code and tested with postman

--- Backend/routes/Authroutes.js
+++ Backend/routes/Authroutes.js
@@ -6,6 +6,8 @@ const User=require('../models/User');
 
 router.post("/signup",async (req,res)=>{
     try{
+        console.log("Signup route hit");          
+  console.log("Request body:", req.body);
     const{username,email,password}=req.body;
     const existinguser=await User.findOne({email});
     if(existinguser)
@@ -13,7 +15,12 @@ router.post("/signup",async (req,res)=>{
     const encrypt=await bcrypt.genSalt(10);
     const hashedPass=await bcrypt.hash(password,encrypt);
     const newu=new User({username,email,password:hashedPass});
+    // try {
     await newu.save();
+    // console.log("User saved successfully:", newu);
+    // } catch (err) {
+    // console.error("Error saving user:", err.message);
+    // }
     res.status(201).json({message:"User created successfully"});
     
     }
@@ -21,6 +28,39 @@ router.post("/signup",async (req,res)=>{
         res.status(500).json({message:"Server error"});
     }
 
+
+});
+
+router.post("/login",async (req,res)=>{
+    
+    try{
+        const{email,password}=req.body;
+    const existuser=await User.findOne({email});
+    if(!existuser)
+        return res.status(400).json({message:"user not found!"});
+    const Matched = await bcrypt.compare(password,existuser.password);
+        if (!Matched) {
+            return res.status(401).json({ message: "Invalid password" });
+        }
+        const token = jwt.sign(
+            { id: existuser._id, email: existuser.email },
+            process.env.JWT_SECRET || "secretKey", 
+            { expiresIn: "1h" }
+        );
+
+        res.status(200).json({
+            message: "Login successful",
+            token,
+            user: {
+                id: existuser._id,
+                username: existuser.username,
+                email: existuser.email
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server error" });
+    }
 
 });
 module.exports = router;
--- Backend/server.js
+++ Backend/server.js
@@ -7,9 +7,7 @@ const app = express();
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB connected"))
   .catch(err => console.error("❌ MongoDB connection error:", err));
-
+app.use(express.json());
 app.get('/', (req, res) => res.send("API running..."));
 app.use('/api/auth', authroutes);
-app.use(express.json());
-app.get('/api/auth/signup',(req,res)=>res.send("hey"));
 app.listen(process.env.PORT, () => console.log(`🚀 Server running on port ${process.env.PORT}`));
