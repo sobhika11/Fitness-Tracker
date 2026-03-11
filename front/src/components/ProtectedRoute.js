e8fdf228ea84f62ea6c414573a26f9ae2019c821
Divyadharshini18 <divyasadhu725@gmail.com>
2026-03-11 22:27:01 +0530

    Frontend design

--- front/src/components/ProtectedRoute.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-
-const ProtectedRoute = ({ children }) => {
-  const isLoggedIn = localStorage.getItem("userEmail");
-  return isLoggedIn ? children : <Navigate to="/" />;
-};
-
-export default ProtectedRoute;
